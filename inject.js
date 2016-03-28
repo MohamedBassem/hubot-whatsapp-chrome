6e9257a4d93d85ff3df5f63994a1ac1d97b98613
MohamedBassem <medoox240@gmail.com>
2016-03-28 11:42:22 +0200

    Making the message more descriptive

--- inject.js
+++ inject.js
@@ -60,7 +60,13 @@ function main(reactGlobal) {
       if(currentChat.unreadCount != 0 && !readMsgs[lastMessage.id._id]){
         console.log("Found a converstation that needs a response : '" + lastMessage.body + "' in " + currentChat.id );
         readMsgs[lastMessage.id._id] = true;
-        window.postMessage({type: "NEW_MESSAGE", message: lastMessage.body, convId: currentChat.id }, "*");
+        window.postMessage({
+            type: "NEW_MESSAGE",
+            message: lastMessage.body,
+            senderId: lastMessage.sender,
+            senderName: lastMessage.senderObj.formattedUser.replace(/ /g, ""),
+            convId: currentChat.id
+        }, "*");
       }
     }
 
